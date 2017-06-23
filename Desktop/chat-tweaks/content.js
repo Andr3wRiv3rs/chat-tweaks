400b9a1ff68855a3d55445d3cb5d2909e3a89b46
Andres <andrewarivers@gmail.com>
2017-06-23 17:19:57 -0600

    1.3

---
+++ Desktop/chat-tweaks/content.js
@@ -0,0 +1,189 @@
+/*
+
++---------------------------------------------------------------------------------------+
+| If you like the extension, consider donating here: https://www.paypal.me/andrewrivers |
+| All donations are much appreciated.                                                   |
++---------------------------------------------------------------------------------------+
+
+*/
+
+img_r = /(https?:\/\/.[^<>h\s]*[^<>t\s]*[^<>t\s]*[^<>p\s]*[^<>:\s]*\.(?:png|jpg|jpeg|gif|svg))/i;
+gyazo_r = /(https?:\/\/.?gyazo\.com\/.[^<>:\s]*)/i;
+//twt_r = /(https?:\/\/.?twitter\.com\/.[^<>"]*\/status\/.[0-9][^<>"]*)/i
+yt_r = /(http(?:s)?:\/\/www\.youtube\.com\/watch\?v=*\S+)/i;
+yt2_r = /(https:\/\/youtu\.be\/*\S+)/i;
+link = /http:\/\/[^" <>]*\.[^" <>]*/i;
+link2 = /https:\/\/gyazo[^" <>]*\.[^" <>]*/i;
+
+prof_r = /(\bfuck*\S+|4r5e|\b5h1t|\b5hit|\ba55|\banal|\banus|\bar5e|\barrse|\barse|\bass|\bass-fucker|\basses|\bassfucker|\bassfukka|\basshole|\bassholes|\basswhole|\ba_s_s|\bb!tch|\bb00bs|\bb17ch|\bb1tch|\bballbag|\bballs|\bballsack|\bbastard|\bbeastial|\bbeastiality|\bbellend|\bbestial|\bbestiality|\bbich|\bbiatch|\bbitch|\bbitcher|\bbitchers|\bbitches|\bbitchin|\bbitching|\bbloody|\bblowjob|\bblowjob|\bblowjobs|\bboiolas|\bbollock|\bbollok|\bboner|\bboob|\bboobs|\bbooobs|\bboooobs|\bbooooobs|\bbooooooobs|\bbreasts|\bbuceta|\bbugger|\bbunnyfucker|\bbutthole|\bbuttmuch|\bbuttplug|\bc0ck|\bc0cksucker|\bcarpetmuncher|\bcawk|\bchink|\bcipa|\bcl1t|\bclit|\bclitoris|\bclits|\bcnut|\bcock|\bcock-sucker|\bcockface|\bcockhead|\bcockmunch|\bcockmuncher|\bcocks|\bcocksuck|\bcocksucked|\bcocksucker|\bcocksucking|\bcocksucks|\bcocksuka|\bcocksukka|\bcokmuncher|\bcoksucka|\bcoon|\bcox|\bcrap|\bcum|\bcummer|\bcumming|\bcums|\bcumshot|\bcunilingus|\bcunillingus|\bcunnilingus|\bcunt|\bcuntlick|\bcuntlicker|\bcuntlicking|\bcunts|\bcyalis|\bcyberfuc|\bcyberfuck|\bcyberfucked|\bcyberfucker|\bcyberfuckers|\bcyberfucking|\bd1ck|\bdamn|\bdick|\bdickhead|\bdildo|\bdildos|\bdlck|\bdog-fucker|\bdoggin|\bdogging|\bdonkeyribber|\bdoosh|\bduche|\bdyke|\bejaculate|\bejaculated|\bejaculates|\bejaculating|\bejaculatings|\bejaculation|\bejakulate|\bfuck|\bfucker|\bf4nny|\bfag|\bfagging|\bfaggitt|\bfaggot|\bfaggs|\bfagot|\bfagots|\bfags|\bfanny|\bfannyflaps|\bfannyfucker|\bfanyy|\bfatass|\bfcuk|\bfcuker|\bfcuking|\bfeck|\bfecker|\bfelching|\bfellate|\bfellatio|\bfingerfuck|\bfingerfucked|\bfingerfucker|\bfingerfuckers|\bfingerfucking|\bfingerfucks|\bfistfuck|\bfistfucked|\bfistfucker|\bfistfuckers|\bfistfucking|\bfistfuckings|\bfistfucks|\bflange|\bfook|\bfooker|\bfuck|\bfucka|\bfucked|\bfucker|\bfuckers|\bfuckhead|\bfuckheads|\bfuckin|\bfucking|\bfuckings|\bfuckingshitmotherfucker|\bfuckme|\bfucks|\bfuckwhit|\bfuckwit|\bfudgepacker|\bfudgepacker|\bfuk|\bfuker|\bfukker|\bfukkin|\bfuks|\bfukwhit|\bfukwit|\bfux|\bfux0r|\bf_u_c_k|\bgangbang|\bgangbanged|\bgangbangs|\bgaylord|\bgaysex|\bgoatse|\bGod|\bgod-dam|\bgod-damned|\bgoddamn|\bgoddamned|\bhardcoresex|\bheshe|\bhoar|\bhoare|\bhoer|\bhore|\bhorniest|\bhorny|\bhotsex|\bjack-off|\bjackoff|\bjerk-off|\bjism|\bjiz|\bjizm|\bjizz|\bkawk|\bknob|\bknobead|\bknobed|\bknobend|\bknobhead|\bknobjocky|\bknobjokey|\bkock|\bkondum|\bkondums|\bkum|\bkummer|\bkumming|\bkums|\bkunilingus|\bl3ich|\bl3itch|\blabia|\blmfao|\blust|\blusting|\bm0f0|\bm0fo|\bm45terbate|\bma5terb8|\bma5terbate|\bmasochist|\bmaster-bate|\bmasterb8|\bmasterbat*|masterbat3|\bmasterbate|\bmasterbation|\bmasterbations|\bmasturbate|\bmo-fo|\bmof0|\bmofo|\bmothafuck|\bmothafucka|\bmothafuckas|\bmothafuckaz|\bmothafucked|\bmothafucker|\bmothafuckers|\bmothafuckin|\bmothafucking|\bmothafuckings|\bmothafucks|\bmotherfucker|\bmotherfuck|\bmotherfucked|\bmotherfucker|\bmotherfuckers|\bmotherfuckin|\bmotherfucking|\bmotherfuckings|\bmotherfuckka|\bmotherfucks|\bmuff|\bmuthafecker|\bmuthafuckker|\bmutherfucker|\bn1gga|\bn1gger|\bnigg3r|\bnigg4h|\bnigga|\bniggah|\bniggas|\bniggaz|\bnigger|\bniggers|\bnobjokey|\bnobhead|\bnobjocky|\bnobjokey|\bnumbnuts|\bnutsack|\borgasim|\borgasims|\borgasm|\borgasms|\bp0rn|\bpawn|\bpecker|\bpenis|\bpenisfucker|\bphonesex|\bphuck|\bphuk|\bphuked|\bphuking|\bphukked|\bphukking|\bphuks|\bphuq|\bpigfucker|\bpimpis|\bpiss|\bpissed|\bpisser|\bpissers|\bpisses|\bpissflaps|\bpissin|\bpissing|\bpissoff|\bpoop|\bporn|\bporno|\bpornography|\bpornos|\bprick|\bpricks|\bpron|\bpube|\bpusse|\bpussi|\bpussies|\bpussy|\bpussys|\brectum|\bretard|\brimjaw|\brimming|\bshit|\bsob|\bsadist|\bschlong|\bscrewing|\bscroat|\bscrote|\bscrotum|\bsemen|\bsex|\bsh!|sh!t|\bsh1t|\bshag|\bshagger|\bshaggin|\bshagging|\bshemale|\bshit|\bshitdick|\bshite|\bshited|\bshitey|\bshitfuck|\bshitfull|\bshithead|\bshiting|\bshitings|\bshits|\bshitted|\bshitter|\bshitters|\bshitting|\bshittings|\bshitty|\bskank|\bslut|\bsluts|\bsmegma|\bsmut|\bsnatch|\bson-of-a-bitch|\bspac|\bspunk|\bs_h_i_t|\bt1tt1e5|\bt1tties|\bteets|\bteez|\btestical|\btesticle|\btit\b|\btitfuck|\btits|\btitt|\btittie5|\btittiefucker|\btitties|\btittyfuck|\btittywank|\btitwank|\btosser|\bturd|\btw4t|\btwat|\btwathead|\btwatty|\btwunt|\btwunter|\bv14gra|\bv1gra|\bvagina|\bviagra|\bvulva|\bw00se|\bwang|\bwank|\bwanker|\bharamebe|\bwanky|\bwhoar|\bwhore|\bwillies|\bwilly|\bxrated|\bxxx|\bharambe|\bnutshack|\broblox*\S+|fnaf*\S+)/i
+
+if(localStorage["prof_str"] == undefined || localStorage["prof_str"] == null || localStorage["prof_str"] == "undefined" || 			localStorage["prof_str"] == ''){
+	localStorage["prof_str"] = 'CENSORED';
+}
+
+if(localStorage["img_"] == undefined || localStorage["img_"] == null || localStorage["img_"] == "undefined" || localStorage["img_"] == ''){
+	localStorage["img_"] = true;
+}
+
+if(localStorage["yt_"] == undefined || localStorage["yt_"] == null || localStorage["yt_"] == "undefined" || localStorage["yt_"] == ''){
+	localStorage["yt_"] = true;
+}
+
+if(localStorage["prof_"] == undefined || localStorage["prof_"] == null || localStorage["prof_"] == "undefined" || localStorage["prof_"] == ''){
+	localStorage["prof_"] = false;
+}
+
+/*
+if(localStorage["twt_"] == undefined || localStorage["twt_"] == null || localStorage["twt_"] == "undefined" || localStorage["twt_"] == ''){
+	localStorage["twt_"] = true;
+}
+*/
+
+$(document).on( 'DOMNodeInserted', function(e) {
+	if(e.target.className == "chat-window-message-content-wrap"){
+		setTimeout(function(){
+
+		hh = 'height:'+$('.chat-window-main').height()*0.3 + 'px;';
+		function test(reg){
+			return reg.test($(e.target).find('P')[0].innerHTML);
+		}
+			
+		if(localStorage["img_"] == 'true' && test(img_r)){
+			img = img_r.exec($(e.target).find('P')[0].innerHTML);
+
+			if(img.length > 0){
+				$(e.target).find('P').append('<br><img class="web-preview" style="margin:20px; margin-left:0px; '+hh+'" src="' + img[0]+'"/><br>');
+
+				$('.chat-window-output')[0].scrollTop += $('.chat-window-main').height()*0.4 + 200;
+			}
+		} else if(localStorage["yt_"] == 'true' && test(yt_r)){
+			for(a in yt_r.exec($(e.target).find('P')[0].innerHTML)){
+				if(a>=1){
+					h = $('.chat-window-main').height()*0.4;
+					yt = yt_r.exec($(e.target).find('P')[0].innerHTML)[a].replace('watch?v=', 'embed/');
+					$(e.target).find('P').append('<br><iframe style="margin:20px; margin-left:0px;border:none;outline:none;" width="'+h*1.7777+'" height="'+h+'" src="'+yt+'?iv_load_policy=3" frameborder="0" allowfullscreen></iframe>');
+					$('.chat-window-output')[0].scrollTop += $('.chat-window-main').height()*0.4 + 200;
+				}
+			}
+		} else if (localStorage["yt_"] == 'true' && test(yt2_r)){
+			for(a in yt2_r.exec($(e.target).find('P')[0].innerHTML)){
+				if(a>=1){
+					h = $('.chat-window-main').height()*0.4;
+					yt = yt2_r.exec($(e.target).find('P')[0].innerHTML)[a].replace('youtu.be/', 'www.youtube.com/embed/');
+					$(e.target).find('P').append('<br><iframe style="margin:20px; margin-left:0px;border:none;outline:none;" width="'+h*1.7777+'" height="'+h+'" src="'+yt+'?iv_load_policy=3" frameborder="0" allowfullscreen></iframe>');
+					$('.chat-window-output')[0].scrollTop += $('.chat-window-main').height()*0.4 + 200;
+				}
+			}
+		} else if(localStorage["img_"] == 'true'){
+			if(test(link) || test(link2)){
+				function getURL(){
+				$.get(link.exec(e.target.innerHTML)[0], function(_a){
+					_a = _a.replace(/src="./g, 'src="'+link.exec(e.target.innerHTML)[0]).replace(/src="\/\//g, 'src="http://');
+					_a = _a.replace(/<head>*<\/head>/g, '').replace(/<script>*<\/script>/g, '');
+					_a = _a.replace(/[^ /]*\.(html|php|xml|htm|xhtml)/g, '');
+
+					img = img_r.exec(_a);
+
+					if(img!=null&&img!=undefined){if(img.length > 0){
+						$(e.target).find('P').append('<br><img class="web-preview" style="margin:20px; margin-left:0px; '+hh+'" src="' + img[0]+'"/><br>');
+
+						$('.chat-window-output')[0].scrollTop += $('.chat-window-main').height()*0.4 + 200;
+					}}
+				});
+				}getURL();
+			}
+		}
+
+		}, 10);
+		
+		
+		if(localStorage["prof_"] == 'true' && test(prof_r)){
+			for(a in prof_r.exec($(e.target).find('P')[0].innerHTML)){
+				$(e.target).find('P')[0].innerHTML = $(e.target).find('P')[0].innerHTML.replace(prof_r, ' <span style="background-color:#191919; color:white; font-size:10px; padding-left:4px; padding-right:4px;">'+localStorage["prof_str"]+'</span> ');
+			}
+		}
+	}
+	if(e.target.className == "chat-friends-list"){
+		$('.chat-rooms-list').append('<br><div id="chat-tweaks-options" class="nav-controls"><a class="btn btn-block btn-outline" ui-sref="chat">Chat Options</a></div>');
+		tweaksToggle = 0;
+
+		function chatTweaks(){
+			if(tweaksToggle==0){
+				tweaksToggle=1;
+				$('.chat-rooms-list').append('<ul id="chat-tweaks-menu" class="shell-nav thin" style="text-align:center;"> </ul>');
+				$('#chat-tweaks-menu').append('<li id="chat-tweaks-img" class="btn btn-block no-animate" ng-repeat="room in client.publicRooms track by room.id"><a title="chat-tweaks">Image Preview: '+localStorage['img_']+'</a></li>');
+				$('#chat-tweaks-menu').append('<li id="chat-tweaks-yt" class="btn btn-block no-animate" ng-repeat="room in client.publicRooms track by room.id"><a title="chat-tweaks">YouTube Preview: '+localStorage['yt_']+'</a></li>');
+				$('#chat-tweaks-menu').append('<li id="chat-tweaks-prof" class="btn btn-block no-animate" ng-repeat="room in client.publicRooms track by room.id"><a title="chat-tweaks">Profanity Filter: '+localStorage['prof_']+'</a></li>');
+				$('#chat-tweaks-menu').append('<li id="chat-tweaks-prof_str" class="btn btn-block no-animate" ng-repeat="room in client.publicRooms track by room.id"><a title="chat-tweaks">Profanity Censor</a></li>');
+				$('#chat-tweaks-menu').append('<li onClick="function openInNewTab(url) {var win = window.open(url, &#39;_blank&#39;);win.focus();}openInNewTab(&#39;https://www.paypal.me/andrewrivers&#39;)" class="btn btn-block no-animate" ng-repeat="room in client.publicRooms track by room.id"><a title="chat-tweaks">Donate</li>');
+				
+				$('#chat-tweaks-menu>li').css('background-color','rgba(0,0,0,0)');
+				$('#chat-tweaks-menu>li>a').css('background-color','rgba(20,20,20,1)');
+
+				function toggle(var_t){
+					if(localStorage[var_t] == 'true'){
+						localStorage[var_t] = 'false';
+					} else {localStorage[var_t] = 'true'}
+
+					$('#chat-tweaks-menu').remove();
+					$('.chat-rooms-list').append('<ul id="chat-tweaks-menu" class="shell-nav thin" style="text-align:center;"> </ul>');
+					$('#chat-tweaks-menu').append('<li id="chat-tweaks-img" class="btn btn-block no-animate" ng-repeat="room in client.publicRooms track by room.id"><a title="chat-tweaks">Image Preview: '+localStorage['img_']+'</a></li>');
+					$('#chat-tweaks-menu').append('<li id="chat-tweaks-yt" class="btn btn-block no-animate" ng-repeat="room in client.publicRooms track by room.id"><a title="chat-tweaks">YouTube Preview: '+localStorage['yt_']+'</a></li>');
+					$('#chat-tweaks-menu').append('<li id="chat-tweaks-prof" class="btn btn-block no-animate" ng-repeat="room in client.publicRooms track by room.id"><a title="chat-tweaks">Profanity Filter: '+localStorage['prof_']+'</a></li>');
+					$('#chat-tweaks-menu').append('<li id="chat-tweaks-prof_str" class="btn btn-block no-animate" ng-repeat="room in client.publicRooms track by room.id"><a title="chat-tweaks">Profanity Censor</a></li>');
+					$('#chat-tweaks-menu').append('<li onClick="function openInNewTab(url) {var win = window.open(url, &#39;_blank&#39;);win.focus();}openInNewTab(&#39;https://www.paypal.me/andrewrivers&#39;);" class="btn btn-block no-animate" ng-repeat="room in client.publicRooms track by room.id"><a title="chat-tweaks">Donate</li>');
+
+					$('#chat-tweaks-menu>li').css('background-color','rgba(0,0,0,0)');
+					$('#chat-tweaks-menu>li>a').css('background-color','rgba(20,20,20,1)');
+
+					$('#chat-tweaks-prof').on('click', function(){toggle('prof_');});
+					$('#chat-tweaks-yt').on('click', function(){toggle('yt_');});
+					$('#chat-tweaks-img').on('click', function(){toggle('img_');});
+					$('#chat-tweaks-prof_str').on('click', function(){
+						localStorage["prof_str"] = prompt('New Censor?');
+						if(localStorage["prof_str"] == 'null'){
+							localStorage["prof_str"] = 'CENSORED';
+						}
+					});
+				}
+				$('#chat-tweaks-prof').on('click', function(){toggle('prof_');});
+				$('#chat-tweaks-yt').on('click', function(){toggle('yt_');});
+				$('#chat-tweaks-img').on('click', function(){toggle('img_');});
+				$('#chat-tweaks-prof_str').on('click', function(){
+					localStorage["prof_str"] = prompt('New Censor?');
+					if(localStorage["prof_str"] == 'null'){
+						localStorage["prof_str"] = 'CENSORED';
+					}
+				});
+			} else{
+				$('#chat-tweaks-menu').remove();
+				tweaksToggle=0;
+			}
+		}
+
+		$('#chat-tweaks-options').on('click', chatTweaks);
+	}
+});
+
+function $loop( delay, callback, int ){
+	c = 0;
+	function loop(){
+		if(int != null && int != undefined & int != 'undefined'){
+			if(c < int){
+				c++;
+				callback();
+				setTimeout(loop, delay);
+			} else {
+				c = 0;
+			}
+		} else {
+			setTimeout(loop, delay);
+		}
+	}
+	loop();
+}
+
+$loop( 10, function(){
+	window.localStorage["chat-room-storage:rooms"] = '{"action":{"type":"join","roomId":175510},"rooms":[2,89211],"time":1485302400103}';
+	 
+},99999999)
